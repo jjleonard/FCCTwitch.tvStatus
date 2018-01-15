f113f347d3a903189a373a2da544f1b4d9c95917
John Leonard <jjleonard@gmail.com>
2018-01-15 21:51:55 +0000

    R1D14

--- js/twt.js
+++ js/twt.js
@@ -2,6 +2,19 @@ const endpoint = "https://wind-bow.gomix.me/twitch-api/";
 const channelsList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 $(document).ready(function(){
+  $(".show-all").click(function(){
+    $(".offline").fadeIn(200);
+    $(".online").fadeIn(200);
+  });
+  $(".show-online").click(function(){
+    $(".offline").fadeOut(200);
+    $(".online").fadeIn(200);
+  });
+  $(".show-offline").click(function(){
+    $(".online").fadeOut(200);
+    $(".offline").fadeIn(200);
+  });
+
   for (i=0; i<channelsList.length; i++){
     getTwitchData(channelsList[i]);
   };
@@ -20,6 +33,11 @@ function getTwitchData(channel){
 };
 
 function showData(e, streaming){
-  var li = "<li class = 'list-group-item'>"+e.name+": <span class='badge'>"+streaming+"</span>: <a href='"+e.url+"'>"+e.status+"</a></li>"
+  if (streaming == "Live"){
+    statusClass = "online";
+  } else {
+    statusClass = "offline";
+  };
+  var li = "<li class = 'list-group-item "+statusClass+"'>"+e.name+": <span class='badge'>"+streaming+"</span>: <a href='"+e.url+"'>"+e.status+"</a></li>"
   $("ul").append(li);
 };
